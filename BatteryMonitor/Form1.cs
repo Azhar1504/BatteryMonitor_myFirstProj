bf9b58f716b06c9417532a6ba226b90cf238a540
azhar bhuheekhan <azharbhuheekhan@hotmail.com>
2019-10-21 09:00:52 +0400

    adding functionalities to the battery monitor

--- BatteryMonitor/Form1.cs
+++ BatteryMonitor/Form1.cs
@@ -19,7 +19,39 @@ namespace BatteryMonitor
 
         private void button1_Click(object sender, EventArgs e)
         {
+            this.Close();
+        }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PowSrc.Text=GetPowerSource();
+            BatStat.Text = GetBatteryStatus().ToString();
+        }
+
+        public String GetPowerSource()
+        {
+            string strPowerLineStatus = "Default";
+            // Getting the current system power status.
+            switch (SystemInformation.PowerStatus.PowerLineStatus)
+            {
+                case PowerLineStatus.Offline:
+                    strPowerLineStatus = "Battery";
+                    break;
+                case PowerLineStatus.Online:
+                    strPowerLineStatus = "AC Power";
+                    break;
+                case PowerLineStatus.Unknown:
+                    strPowerLineStatus = "Unknown";
+                    break;
+            }
+            return strPowerLineStatus;
+        }
+        public float GetBatteryStatus()
+        {
+            float batterylife;
+            batterylife = SystemInformation.PowerStatus.BatteryLifePercent;
+            batterylife *= 100.0f;
+            return batterylife;
         }
     }
 }
